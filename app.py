ef173b87242e666230f99c8cb85294999fecc2e3
Sandali Gunathilaka <sandaligunathilaka99@gmail.com>
2025-03-03 08:52:09 +0530

    app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def main():
     # HTML for styling
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">SEWING TARGET TIME PREDICTOR</h2>
+    <h2 style="color:white;text-align:center;">TARGET SEWING TIME PREDICTOR</h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
